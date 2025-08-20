20b5122aa050272eee11f7f16c9b93cde27a4429
Mohammed Junaith A <mohammed@Mohammeds-MacBook-Air.local>
2025-08-20 15:01:36 +0530

    favicon updated

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteUrl: "https://junaith-portfolio.vercel.app/projects",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+  changefreq: "weekly",
+  priority: 0.7,
+};
