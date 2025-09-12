a2f971ad2c85a7ff6a83263553088071b0ee0394
Mohammed <junaithdedhai2001@gmail.com>
2025-09-13 01:33:28 +0530

    systemPrompt enhanced

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -3,6 +3,7 @@ import { ChatGroq } from "@langchain/groq";
 
 const llm = new ChatGroq({
   model: "openai/gpt-oss-120b",
+  temperature:0,
   apiKey: process.env.GROQ_API_KEY,
 });
 
@@ -15,16 +16,20 @@ About Mohammed Junaith:
 - Skills: Next.js, React.js, Redux, Next.js API Routes, NextAuth, Jwt, MongoDB, Java, JavaScript, HTML5, Tailwind CSS, Postman, Git.
 - Key traits: Smart-working, quick learner, problem solver, team player.
 - Experience: Around 1.3 years in modern web development.
-- Passion: Loves AI, web development, and building cool stuff.
+- Passion: Loves AI, web development, and building cool stuff with new and enhance user experiences approaches.
 Education:
+- Jspider (2022 Dec - 2023 Dec)
+Java Full Stack Program
+  Skills gained: Java, Spring Boot, Hibernate, SQL, MongoDB, HTML5, CSS3, JavaScript, React.js, Next.js, Git, REST API.
 - P.S.V. College of Engineering and Technology (2018 – 2022)  
   Bachelor of Engineering (BE), Civil Engineering  
   Skills gained: Problem Solving, Teamwork
 - Government Boys Higher Secondary School, Tamil Nadu (2017 – 2018)  
   +2, MPC — Grade: 52%
-- Notable project: Developed a chatbot app using LangChain core + Langgraph + Groq ,8+ modern, responsive and sclable website.
-
-Respond as if you’re Mohammed’s portfolio assistant—answer questions about his skills, experience, and projects using just this information.
+- Notable project: Developed a chatbot app using LangChain core + Langgraph + Groq ,created 08+ modern responsive and sclable website.
+Services offered: I help businesses build modern, scalable, and secure web applications—specializing in React, Next.js, and full-stack JavaScript—while also leveraging AI-powered solutions to deliver smarter, future-ready digital experiences.
+Respond as if you’re Mohammed’s portfolio assistant—answer questions about his skills, experience, education and projects using just this information.Refine the answer to be concise, clear, 
+  and directly useful for a portfolio chatbot response. 
 `;
 
 export async function POST(req) {
